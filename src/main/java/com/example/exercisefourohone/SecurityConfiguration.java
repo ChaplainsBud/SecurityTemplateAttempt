c31bcb6fbc648cc9320984518774d726bc0fccd8
Matthew <matthew.robert.leroe@gmail.com>
2020-03-02 21:32:13 -0500

    almost done 4.04

--- src/main/java/com/example/exercisefourohone/SecurityConfiguration.java
+++ src/main/java/com/example/exercisefourohone/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.example.exercisefourohone;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -18,32 +21,52 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Autowired
+    private SSUserDetailsService userDetailsService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception{
+        return new SSUserDetailsService(userRepository);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                     .authorizeRequests()
-                    .antMatchers("/").access("hasAnyAuthority('USER','ADMIN')")
-
-                    .antMatchers("/course").access("hasAnyAuthority('USER', 'ADMIN')")
-                    .antMatchers("/student").access("hasAnyAuthority('USER')")
-                    .antMatchers("/teacher").access("hasAnyAuthority('ADMIN')")
-
+                    .antMatchers("/", "/h2-console/**").permitAll()
                     .antMatchers("/admin")
                     .access("hasAuthority('ADMIN')")
                     .anyRequest().authenticated()
                     .and().formLogin().loginPage("/login").permitAll()
                     .and()
                     .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/login").permitAll();
+                    .logoutSuccessUrl("/login").permitAll().permitAll().and().httpBasic();
+
+                     http.csrf().disable();
+                     http.headers().frameOptions().disable();
+
+//                .antMatchers("/").access("hasAnyAuthority('USER','ADMIN')")
+//
+//                    .antMatchers("/course").access("hasAnyAuthority('USER', 'ADMIN')")
+//                    .antMatchers("/student").access("hasAnyAuthority('USER')")
+//                    .antMatchers("/teacher").access("hasAnyAuthority('ADMIN')")
+
+
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-        throws Exception{
-        auth.inMemoryAuthentication().withUser("dave")
-                .password(passwordEncoder().encode("begreat")).authorities("ADMIN").and().withUser("user")
-                .password(passwordEncoder().encode("password"))
-                .authorities("USER");
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+        auth.userDetailsService(userDetailsServiceBean())
+                .passwordEncoder(passwordEncoder());
+//
+//        auth.inMemoryAuthentication().withUser("dave")
+//                .password(passwordEncoder().encode("begreat")).authorities("ADMIN").and().withUser("user")
+//                .password(passwordEncoder().encode("password"))
+//                .authorities("USER");
     }
 }
 
